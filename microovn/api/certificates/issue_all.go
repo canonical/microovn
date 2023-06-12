0ad77dcf9ea7fcd9d910e44432b989b544fd1fda
Martin Kalcok <martin.kalcok@canonical.com>
2023-06-12 14:21:45 +0200

    REST api for OVN certificate management.
    Following new API endpoints were added:
    * 'PUT /certificates/<service>' - reissue new
      certificate for specified OVN <service>
    * 'PUT /certificates' - reissue certificates for
      all OVN services
    * 'PUT /ca' - issue new CA certificate and
      re-issue all OVN services certificates
    
    Signed-off-by: Martin Kalcok <martin.kalcok@canonical.com>
    Signed-off-by: Theodoros Vakalopoulos <theovakalopoulos@gmail.com>

---
+++ microovn/api/certificates/issue_all.go
@@ -0,0 +1,29 @@
+package certificates
+
+import (
+	"net/http"
+
+	"github.com/canonical/microcluster/rest"
+	"github.com/canonical/microcluster/state"
+	"github.com/lxc/lxd/lxd/response"
+	"github.com/lxc/lxd/shared/logger"
+)
+
+// IssueCertificatesAllEndpoint defines endpoint for /1.0/certificates
+var IssueCertificatesAllEndpoint = rest.Endpoint{
+	Path: "certificates",
+	Put:  rest.EndpointAction{Handler: issueCertificatesAllPut, AllowUntrusted: false, ProxyTarget: true},
+}
+
+// issueCertificatesAllPut implements PUT method for /1.0/certificates endpoint. The function issues new
+// certificates for every OVN service enabled on this cluster member.
+func issueCertificatesAllPut(s *state.State, _ *http.Request) response.Response {
+	logger.Info("Re-issuing certificate for all enabled OVN services.")
+	responseData, err := reissueAllCertificates(s)
+	if err != nil {
+		logger.Errorf("failed to issue certificates for all services: %w", err)
+		return response.ErrorResponse(500, "Internal server error.")
+	}
+
+	return response.SyncResponse(true, responseData)
+}
