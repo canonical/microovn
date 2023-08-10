806c22397cf39bbfd5cd1b6d9c007c96aa1ceca3
Martin Kalcok <martin.kalcok@canonical.com>
2023-08-10 18:10:00 +0200

    Run cluster and TLS tests after upgrade
    Using method of executing `bats` from within tests,
    execute basic cluster and tls tests after running an upgrade.
    
    Signed-off-by: Martin Kalcok <martin.kalcok@canonical.com>

--- tests/test_helper/setup_teardown/upgrade.bash
+++ tests/test_helper/setup_teardown/upgrade.bash
@@ -5,19 +5,38 @@ setup_file() {
     load ../.bats/bats-support/load.bash
     load ../.bats/bats-assert/load.bash
 
+    ABS_TOP_TEST_DIRNAME="${BATS_TEST_DIRNAME}/"
+    export ABS_TOP_TEST_DIRNAME
+
     # Env variable MICROOVN_SNAP_CHANNEL must be specified for tests to know
     # from which channel should be MicroOVN installed before upgrading
     assert [ -n "$MICROOVN_SNAP_CHANNEL" ]
 
-    TEST_CONTAINERS=$(container_names "$BATS_TEST_FILENAME" 3)
+    TEST_CONTAINERS=$(container_names "$BATS_TEST_FILENAME" 4)
+    CENTRAL_CONTAINERS=""
+    CHASSIS_CONTAINERS=""
+
     export TEST_CONTAINERS
+    export CENTRAL_CONTAINERS
+    export CHASSIS_CONTAINERS
+
     launch_containers jammy $TEST_CONTAINERS
     wait_containers_ready $TEST_CONTAINERS
     install_microovn_from_store "$MICROOVN_SNAP_CHANNEL" $TEST_CONTAINERS
     bootstrap_cluster $TEST_CONTAINERS
 
-    # detect and export initial MicroOVN snap revision
+    # Categorize containers as "CENTRAL" and "CHASSIS" based on the services they run
     local container=""
+    for container in $TEST_CONTAINERS; do
+        container_services=$(microovn_get_cluster_services "$container")
+        if [[ "$container_services" == *"central"* ]]; then
+            CENTRAL_CONTAINERS+="$container "
+        else
+            CHASSIS_CONTAINERS+="$container "
+        fi
+    done
+
+    # detect and export initial MicroOVN snap revision
     container=$(echo "$TEST_CONTAINERS" | awk '{print $1;}' )
     export MICROOVN_SNAP_REV=""
     MICROOVN_SNAP_REV=$(lxc_exec "$container" "snap list | grep microovn | awk '{print \$3;}'")
