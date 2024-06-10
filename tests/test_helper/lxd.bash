6dfa0f9c562649dc921415b7bca68be7bbc681b7
Frode Nordahl <frode.nordahl@canonical.com>
2024-06-10 13:31:02 +0200

    tests/lxd: Add helper accepting arguments to launch command.
    Groups of tests may have special requirements for test containers
    that we do not want to generally enable for other tests.
    
    One example is ``security.nesting``, which we need for data path
    tests, and at the same time we would want to ensure the other
    tests can pass without it being enabled.
    
    The ``lxc`` command merges config passed through command line
    arguments and read from YAML on stdin, which enables us to add
    additional config at runtime without modifying the base instance
    config.
    
    Signed-off-by: Frode Nordahl <frode.nordahl@canonical.com>

--- tests/test_helper/lxd.bash
+++ tests/test_helper/lxd.bash
@@ -1,11 +1,23 @@
-function launch_containers() {
-    local containers=$*
+function launch_containers_args() {
+    local launch_args=$1; shift
+    local containers=$*; shift
+
     local image="${MICROOVN_TEST_CONTAINER_IMAGE:-ubuntu:lts}"
+
     for container in $containers; do
         echo "# Launching '$image' container: $container" >&3
-        lxc launch -q "$image" "$container" < \
+        # we actually want word splitting for launch_args to expand multiple
+        # arguments and allow empty string.
+        # shellcheck disable=SC2086
+        lxc launch -q "$image" "$container" $launch_args < \
             "$BATS_TEST_DIRNAME/lxd-instance-config.yaml"
     done
+}
+
+function launch_containers() {
+    local containers=$*
+
+    launch_containers_args "" "$containers"
 }
 
 function create_lxd_network() {
