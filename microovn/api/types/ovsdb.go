0c6a244b7685b8db3efe702f4caed756999b952e
Martin Kalcok <martin.kalcok@canonical.com>
2024-06-11 23:16:52 +0200

    API: Endpoint for expected OVSDB schema versions on all nodes
    This change adds MicroOVN API endpoint and associated client function
    that allows fetching a collection of expected OVSDB schema versions
    from each node in the deployment.
    
    Signed-off-by: Martin Kalcok <martin.kalcok@canonical.com>

--- microovn/api/types/ovsdb.go
+++ microovn/api/types/ovsdb.go
@@ -9,3 +9,14 @@ const (
 	OvsdbSchemaFetchErrorGeneric                                   // General catch-all error that did not fit more specific definition
 	OvsdbSchemaFetchErrorNotSupported                              // API endpoint returned 404, signaling that the node does not support it.
 )
+
+// OvsdbSchemaReport is just a collection of OvsdbSchemaVersionResult structs
+type OvsdbSchemaReport = []OvsdbSchemaVersionResult
+
+// OvsdbSchemaVersionResult is a rich representation of a schema result fetch. It encapsulates node's Hostname,
+// OVSDB schema version, and whether there were any error while fetching data from this node.
+type OvsdbSchemaVersionResult struct {
+	Host          string                `json:"host"`
+	SchemaVersion string                `json:"schemaVersion"`
+	Error         OvsdbSchemaFetchError `json:"error"`
+}
