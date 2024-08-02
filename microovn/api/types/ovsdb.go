1ee21adb7dd96c45369c88f12159869999c4d6d3
Frode Nordahl <frode.nordahl@canonical.com>
2024-08-02 20:17:40 +0200

    treewide: Fix form of comments for exported vars.
    Comments for exported variables should include the variable name.
    
    Signed-off-by: Frode Nordahl <frode.nordahl@canonical.com>

--- microovn/api/types/ovsdb.go
+++ microovn/api/types/ovsdb.go
@@ -5,9 +5,14 @@ package types
 type OvsdbSchemaFetchError int
 
 const (
-	OvsdbSchemaFetchErrorNone         OvsdbSchemaFetchError = iota // No error occurred
-	OvsdbSchemaFetchErrorGeneric                                   // General catch-all error that did not fit more specific definition
-	OvsdbSchemaFetchErrorNotSupported                              // API endpoint returned 404, signaling that the node does not support it.
+	// OvsdbSchemaFetchErrorNone         - No error occurred.
+	OvsdbSchemaFetchErrorNone OvsdbSchemaFetchError = iota
+	// OvsdbSchemaFetchErrorGeneric      - General catch-all error that did not
+	//                                     fit more specific definition.
+	OvsdbSchemaFetchErrorGeneric
+	// OvsdbSchemaFetchErrorNotSupported - API endpoint returned 404, signaling
+	//                                     that the node does not support it.
+	OvsdbSchemaFetchErrorNotSupported
 )
 
 // OvsdbSchemaReport is just a collection of OvsdbSchemaVersionResult structs
