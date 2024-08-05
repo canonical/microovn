canonical/microovn

commit 3d77b1ea6e658c1ffa47c1aaaa7d17ca253608a3

REST api for OVN certificate management.
Following new API endpoints were added:
* 'PUT /certificates/<service>' - reissue new
  certificate for specified OVN <service>
* 'PUT /certificates' - reissue certificates for
  all OVN services
* 'PUT /ca' - issue new CA certificate and
  re-issue all OVN services certificates

Signed-off-by: Martin Kalcok <martin.kalcok@canonical.com>

microovn/api/certificates/regenerate_ca.go
@@ -1,0 +1,86 @@
+package certificates
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/canonical/microcluster/client"
+	"github.com/canonical/microcluster/rest"
+	"github.com/canonical/microcluster/state"
+	"github.com/lxc/lxd/lxd/response"
+	"github.com/lxc/lxd/shared/logger"
+
+	"github.com/canonical/microovn/microovn/api/types"
+	microovnClient "github.com/canonical/microovn/microovn/client"
+	"github.com/canonical/microovn/microovn/ovn"
+)
+
+// RegenerateCaEndpoint defines endpoint for /1.0/ca
+var RegenerateCaEndpoint = rest.Endpoint{
+	Path: "ca",
+	Put:  rest.EndpointAction{Handler: regenerateCaPut, AllowUntrusted: false, ProxyTarget: true},
+}
+
+// regenerateCaPut implements PUT method for /1.0/ca endpoint. The function issues new CA certificate
+// and triggers re-issue of all service certificates on all MicroOVN cluster members
+func regenerateCaPut(s *state.State, r *http.Request) response.Response {
+	var err error
+	responseData := types.NewRegenerateCaResponse()
+
+	// Check that this is the initial node that received the request and recreate new CA certificate
+	if !client.IsForwardedRequest(r) {
+		// Only one recipient of this request needs to generate new CA
+		logger.Info("Re-issuing CA certificate and private key")
+		err = ovn.GenerateNewCACertificate(s)
+		if err != nil {
+			logger.Errorf("Failed to generate new CA certificate: %w", err)
+			responseData.NewCa = false
+			return response.SyncResponse(false, &responseData)
+		} else {
+			responseData.NewCa = true
+		}
+
+		// Get clients for rest of the cluster members
+		cluster, err := s.Cluster(r)
+		if err != nil {
+			logger.Errorf("Failed to get a client for every cluster member: %w", err)
+			return response.SyncResponse(false, &responseData)
+		}
+
+		// Bump rest of the cluster members to reissue their certificates with new CA
+		err = cluster.Query(s.Context, true, func(ctx context.Context, c *client.Client) error {
+			logger.Infof("Requesting cluster member at '%s' to re-issue its OVN certificates", c.URL())
+			result, err := microovnClient.RegenerateCA(ctx, c)
+			if err != nil {
+				clientURL := c.URL()
+				errMsg := fmt.Sprintf("failed to contact cluster member with address %q: %s", clientURL.String(), err)
+				responseData.Errors = append(responseData.Errors, errMsg)
+			} else {
+				for host, service := range result.ReissuedCertificates {
+					responseData.ReissuedCertificates[host] = service
+				}
+			}
+
+			return nil
+		})
+		if err != nil {
+			return response.SmartError(err)
+		}
+	}
+
+	logger.Info("Re-issuing all local OVN certificates")
+	err = ovn.DumpCA(s)
+	if err != nil {
+		logger.Errorf("%w", err)
+		return response.SyncResponse(false, &responseData)
+	}
+
+	reissuedCertificates, err := reissueAllCertificates(s)
+	if err != nil {
+		logger.Errorf("Failed to reissue certificates with new CA: %w", err)
+	}
+	responseData.ReissuedCertificates[s.Name()] = *reissuedCertificates
+
+	return response.SyncResponse(true, &responseData)
+}

microovn/api/certificates/common.go
@@ -1,0 +1,69 @@
+package certificates
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/canonical/microcluster/state"
+	"github.com/lxc/lxd/shared/logger"
+
+	"github.com/canonical/microovn/microovn/api/types"
+	"github.com/canonical/microovn/microovn/database"
+	"github.com/canonical/microovn/microovn/ovn"
+)
+
+// enabledOvnServices returns list of OVN services enabled on this MicroOVN cluster member.
+func enabledOvnServices(s *state.State) ([]string, error) {
+	var enabledServices []string
+
+	// Get list of existing local OVN services.
+	err := s.Database.Transaction(s.Context, func(ctx context.Context, tx *sql.Tx) error {
+		name := s.Name()
+		services, err := database.GetServices(ctx, tx, database.ServiceFilter{Member: &name})
+		if err != nil {
+			return err
+		}
+
+		for _, srv := range services {
+			if srv.Service == "central" {
+				enabledServices = append(enabledServices, "ovnnb", "ovnsb", "ovn-northd")
+			}
+
+			if srv.Service == "switch" {
+				enabledServices = append(enabledServices, "ovn-controller")
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		enabledServices = nil
+		err = fmt.Errorf("failed to lookup local services eligible for certificate refresh: %s", err)
+	}
+
+	return enabledServices, err
+}
+
+// reissueAllCertificates issues new certificates, using current CA, for every OVN service that is enabled
+// on this MicroOVN cluster member.
+func reissueAllCertificates(s *state.State) (*types.IssueCertificateResponse, error) {
+	responseData := types.IssueCertificateResponse{}
+
+	activeServices, err := enabledOvnServices(s)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, service := range activeServices {
+		err = ovn.GenerateNewServiceCertificate(s, service, ovn.CertificateTypeServer)
+		if err != nil {
+			logger.Errorf("Failed to issue certificate for %s: %s", service, err)
+			responseData.Failed = append(responseData.Failed, service)
+		} else {
+			responseData.Success = append(responseData.Success, service)
+		}
+	}
+
+	return &responseData, nil
+}

microovn/api/certificates/issue.go
@@ -1,0 +1,76 @@
+package certificates
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/canonical/microcluster/rest"
+	"github.com/canonical/microcluster/state"
+	"github.com/gorilla/mux"
+	"github.com/lxc/lxd/lxd/response"
+	"github.com/lxc/lxd/shared/logger"
+
+	"github.com/canonical/microovn/microovn/api/types"
+	"github.com/canonical/microovn/microovn/ovn"
+)
+
+// IssueCertificatesEndpoint defines endpoint for /1.0/certificates/<service-name>.
+var IssueCertificatesEndpoint = rest.Endpoint{
+	Path: "certificates/{service}",
+	Put:  rest.EndpointAction{Handler: issueCertificatesPut, AllowUntrusted: false, ProxyTarget: true},
+}
+
+// issueCertificatesPut implements PUT method for /1.0/certificates/<service-name> endpoint. The function parses
+// service name from the request URL and if the service is currently enabled on this cluster member, it
+// issues new certificate for it.
+func issueCertificatesPut(s *state.State, r *http.Request) response.Response {
+	// Get requested service name
+	requestedService, err := url.PathUnescape(mux.Vars(r)["service"])
+	if err != nil {
+		logger.Errorf("failed to parse service name from URL '%s'", r.URL)
+		return response.ErrorResponse(500, "Internal server error")
+	}
+	logger.Infof("Issuing new certificate for '%s' service.", requestedService)
+
+	// Get all enabled services and make sure that the requested service is among them.
+	eligibleServices, err := enabledOvnServices(s)
+	if err != nil {
+		logger.Errorf("failed to lookup local services eligible for certificate refresh: %s", err)
+		return response.ErrorResponse(500, "Internal server error.")
+	}
+
+	isCertificateAllowed := false
+	for _, service := range eligibleServices {
+		if requestedService == service {
+			isCertificateAllowed = true
+			break
+		}
+	}
+
+	// Fail with 404 if requested service is not enabled
+	if !isCertificateAllowed {
+		missingMsg := fmt.Sprintf(
+			"Can't issue certificate for service '%s'. Service is not enabled on this member. Enabled services: %s",
+			requestedService,
+			strings.Join(eligibleServices, ", "),
+		)
+		logger.Warn(missingMsg)
+		return response.ErrorResponse(404, missingMsg)
+	}
+
+	// Attempt to issue new certificate and return response object
+	err = ovn.GenerateNewServiceCertificate(s, requestedService, ovn.CertificateTypeServer)
+	result := types.IssueCertificateResponse{}
+
+	if err != nil {
+		result.Failed = []string{requestedService}
+		logger.Errorf("failed to reissue certificate for '%s' service: %s", requestedService, err)
+		return response.SyncResponse(false, &result)
+	}
+
+	result.Success = []string{requestedService}
+
+	return response.SyncResponse(true, &result)
+}

microovn/api/certificates/issue_all.go
@@ -1,0 +1,29 @@
+package certificates
+
+import (
+	"net/http"
+
+	"github.com/canonical/microcluster/rest"
+	"github.com/canonical/microcluster/state"
+	"github.com/lxc/lxd/lxd/response"
+	"github.com/lxc/lxd/shared/logger"
+)
+
+// IssueCertificatesAllEndpoint defines endpoint for /1.0/certificates
+var IssueCertificatesAllEndpoint = rest.Endpoint{
+	Path: "certificates",
+	Put:  rest.EndpointAction{Handler: issueCertificatesAllPut, AllowUntrusted: false, ProxyTarget: true},
+}
+
+// issueCertificatesAllPut implements PUT method for /1.0/certificates endpoint. The function issues new
+// certificates for every OVN service enabled on this cluster member.
+func issueCertificatesAllPut(s *state.State, _ *http.Request) response.Response {
+	logger.Info("Re-issuing certificate for all enabled OVN services.")
+	responseData, err := reissueAllCertificates(s)
+	if err != nil {
+		logger.Errorf("failed to issue certificates for all services: %w", err)
+		return response.ErrorResponse(500, "Internal server error.")
+	}
+
+	return response.SyncResponse(true, responseData)
+}

microovn/api/endpoints.go
@@ -3,9 +3,14 @@ package api
 
 import (
 	"github.com/canonical/microcluster/rest"
+
+	"github.com/canonical/microovn/microovn/api/certificates"
 )
 
 // Endpoints is a global list of all API endpoints on the /1.0 endpoint of microovn.
 var Endpoints = []rest.Endpoint{
 	servicesCmd,
+	certificates.IssueCertificatesEndpoint,
+	certificates.IssueCertificatesAllEndpoint,
+	certificates.RegenerateCaEndpoint,
 }

microovn/api/types/certificates.go
@@ -1,0 +1,87 @@
+// Package types provides shared types and structs.
+package types
+
+import "fmt"
+
+// IssueCertificateResponse is a structure that models response to requests for issuance
+// of OVN certificates.
+type IssueCertificateResponse struct {
+	Success []string `json:"success" yaml:"success"` // List of services for which new certificates were issued
+	Failed  []string `json:"failed" yaml:"failed"`   // List of services for which new certificates issue failed
+}
+
+// PrettyPrint method formats and prints contents of IssueCertificateResponse object
+func (i *IssueCertificateResponse) PrettyPrint() {
+	fmt.Println("Local certificates reissued successfully:")
+	if i.Success == nil {
+		fmt.Println("None")
+	} else {
+		for _, service := range i.Success {
+			fmt.Println(service)
+		}
+	}
+
+	if i.Failed != nil {
+		fmt.Println("\nLocal certificates reissue failed:")
+		for _, service := range i.Failed {
+			fmt.Println(service)
+		}
+	}
+}
+
+// RegenerateCaResponse is a structure that models response to requests for complete CA rebuild
+type RegenerateCaResponse struct {
+	NewCa                bool                                `json:"newCa" yaml:"newCa"`                               // True if this node issued new CA cert
+	ReissuedCertificates map[string]IssueCertificateResponse `json:"reissuedCertificates" yaml:"reissuedCertificates"` // map of hosts and service certificates they issued
+	Errors               []string                            `json:"error"`                                            //
+}
+
+// PrettyPrint method formats and prints contents of RegenerateCaResponse object
+func (r *RegenerateCaResponse) PrettyPrint() {
+	var newCaSuccess string
+	if r.NewCa {
+		newCaSuccess = "Issued"
+	} else {
+		newCaSuccess = "Not Issued"
+	}
+
+	fmt.Printf("New CA certificate: %s\n\n", newCaSuccess)
+	fmt.Print("Service certificate re-issued for following services:")
+
+	anyFailure := false
+	for host, certificates := range r.ReissuedCertificates {
+		fmt.Printf("\n[Host %s]\n", host)
+		for _, service := range certificates.Success {
+			fmt.Printf("%s: Success\n", service)
+		}
+		for _, service := range certificates.Failed {
+			fmt.Printf("%s: Failed!\n", service)
+			anyFailure = true
+		}
+	}
+
+	if len(r.Errors) != 0 {
+		anyFailure = true
+		fmt.Println("\n[Errors]")
+		for _, errMsg := range r.Errors {
+			fmt.Println(errMsg)
+		}
+	}
+
+	if anyFailure {
+		fmt.Println(
+			"\n Some of the service certificates failed to be re-issued. You can inspect logs and " +
+				"attempt to re-issue these certificates using microovn CLI on affected cluster members.",
+		)
+	}
+
+}
+
+// NewRegenerateCaResponse returns pointer to initialized RegenerateCaResponse object
+func NewRegenerateCaResponse() *RegenerateCaResponse {
+	return &RegenerateCaResponse{
+		NewCa:                false,
+		ReissuedCertificates: make(map[string]IssueCertificateResponse),
+		Errors:               make([]string, 0),
+	}
+}

microovn/client/client.go
@@ -26,3 +26,50 @@ func GetServices(ctx context.Context, c *client.Client) (types.Services, error) 
 
 	return services, nil
 }
+
+// ReissueCertificate sends request to local MicroOVN cluster member to re-issue new certificate for
+// selected service.
+func ReissueCertificate(ctx context.Context, c *client.Client, serviceName string) (types.IssueCertificateResponse, error) {
+	queryCtx, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+
+	response := types.IssueCertificateResponse{}
+	err := c.Query(queryCtx, "PUT", api.NewURL().Path("certificates", serviceName), nil, &response)
+	if err != nil {
+		return response, fmt.Errorf("failed to reissue certificate: %w", err)
+	}
+
+	return response, nil
+}
+
+// ReissueAllCertificate sends request to local MicroOVN cluster member to re-issue new certificates for every
+// enabled OVN service present.
+func ReissueAllCertificate(ctx context.Context, c *client.Client) (types.IssueCertificateResponse, error) {
+	queryCtx, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+
+	response := types.IssueCertificateResponse{}
+	err := c.Query(queryCtx, "PUT", api.NewURL().Path("certificates"), nil, &response)
+	if err != nil {
+		return response, fmt.Errorf("failed to reissue certificate: %w", err)
+	}
+
+	return response, nil
+}
+
+// RegenerateCA sends request to completely rebuild the OVN PKI. It causes new CA certificate to be issued and shared
+// between MicroOVN cluster members, and it triggers re-issue of all OVN service certificates on all cluster members.
+func RegenerateCA(ctx context.Context, c *client.Client) (types.RegenerateCaResponse, error) {
+	queryCtx, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+
+	response := types.NewRegenerateCaResponse()
+
+	err := c.Query(queryCtx, "PUT", api.NewURL().Path("ca"), nil, &response)
+	if err != nil {
+		return *response, fmt.Errorf("failed to generate new CA: %w", err)
+	}
+
+	return *response, nil
+
+}

commit 3068be92db72451262ac08503b014cc6f7420d9b

certificates: Fix incorrect format string.
Fix an occurence of using `%s` with a non-string argument to
logger.Infof().

Fixes: 3d77b1ea6e65 ("REST api for OVN certificate management.")
Signed-off-by: Frode Nordahl <frode.nordahl@canonical.com>

microovn/.golangci.yml
@@ -3,3 +3,4 @@ linters:
   enable:
     - errcheck
     - gosimple
+    - govet

microovn/api/certificates/regenerate_ca.go
@@ -50,10 +50,10 @@ func regenerateCaPut(s *state.State, r *http.Request) response.Response {
 
 		// Bump rest of the cluster members to reissue their certificates with new CA
 		err = cluster.Query(s.Context, true, func(ctx context.Context, c *client.Client) error {
-			logger.Infof("Requesting cluster member at '%s' to re-issue its OVN certificates", c.URL())
+			clientURL := c.URL()
+			logger.Infof("Requesting cluster member at '%s' to re-issue its OVN certificates", clientURL.String())
 			result, err := microovnClient.RegenerateCA(ctx, c)
 			if err != nil {
-				clientURL := c.URL()
 				errMsg := fmt.Sprintf("failed to contact cluster member with address %q: %s", clientURL.String(), err)
 				responseData.Errors = append(responseData.Errors, errMsg)
 			} else {