3aca953bcedf702b621690adb2515a7d8ef63cbe
MJ Ponsonby <mj.ponsonby@canonical.com>
2024-08-23 15:47:54 +0100

    Tests for service reenablement + central migration
    This improves the central migration tests to ensure the node has
    properly left/joined the clusters on disable/enable.
    This also adds in a new set of tests which restart all the services and
    ensure that the system still has full functionality.
    
    Signed-off-by: MJ Ponsonby <mj.ponsonby@canonical.com>

---
+++ tests/test_helper/setup_teardown/services_restart.bash
@@ -0,0 +1,29 @@
+setup_file() {
+    load test_helper/common.bash
+    load test_helper/lxd.bash
+    load test_helper/microovn.bash
+
+
+    TEST_CONTAINERS=$(container_names "$BATS_TEST_FILENAME" 3)
+    export TEST_CONTAINERS
+    launch_containers $TEST_CONTAINERS
+    wait_containers_ready $TEST_CONTAINERS
+    install_microovn "$MICROOVN_SNAP_PATH" $TEST_CONTAINERS
+    bootstrap_cluster $TEST_CONTAINERS
+
+    for container in $TEST_CONTAINERS; do
+        lxc_exec "$container" "microovn disable central"
+        lxc_exec "$container" "microovn enable central"
+
+        lxc_exec "$container" "microovn disable chassis"
+        lxc_exec "$container" "microovn enable chassis"
+
+        lxc_exec "$container" "microovn disable switch"
+        lxc_exec "$container" "microovn enable switch"
+    done
+
+}
+
+teardown_file() {
+    delete_containers $TEST_CONTAINERS
+}
