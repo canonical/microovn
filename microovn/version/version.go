5ad2d90e44097ead5495870bb69f5dcb1a15b6e6
Frode Nordahl <frode.nordahl@canonical.com>
2024-08-29 09:25:41 +0200

    microovn: Use OVN major version in DaemonArgs.
    At present we provide a git hash as the version in DaemonArgs.
    Other microcloud daemons use this information to determine
    compatibility, so let's use our main component major version
    instead.
    
    Add unit test for the pure MajorVersion function.
    
    Signed-off-by: Frode Nordahl <frode.nordahl@canonical.com>
    Signed-off-by: Theodoros Vakalopoulos <theovakalopoulos@gmail.com>

--- microovn/version/version.go
+++ microovn/version/version.go
@@ -1,6 +1,8 @@
 // Package version provides shared version information.
 package version
 
+import "strings"
+
 // MicroOvnVersion contains version of MicroOVN (set at build time)
 var MicroOvnVersion string
 
@@ -9,3 +11,9 @@ var OvnVersion string
 
 // OvsVersion contains version of 'openvswitch' package used to build MicroOVN (set at build time)
 var OvsVersion string
+
+// MajorVersion extracts the major version of the supplied "version" argument.
+func MajorVersion(version string) string {
+	versionSlice := strings.Split(version, ".")
+	return strings.Join(versionSlice[:min(2, len(versionSlice))], ".")
+}
