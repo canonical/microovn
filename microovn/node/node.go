c73bc9f66f4618224ce146ec64fbc665646a3545
Martin Kalcok <martin.kalcok@canonical.com>
2024-09-05 19:36:48 +0200

    bgp: Resource cleanup on bgp disable
    When users disable bgp service, we need to clean up
    resources that were created for BGP redirection. This
    change utilises "external-ids" column on various
    OVN/OVS resources to tag them when they are created.
    During the teardown, we can then find those resources
    based on the external-ids and remove them.
    
    Signed-off-by: MJ Ponsonby <mj.ponsonby@canonical.com>
    Signed-off-by: Martin Kalcok <martin.kalcok@canonical.com>

--- microovn/node/node.go
+++ microovn/node/node.go
@@ -74,7 +74,7 @@ func DisableService(ctx context.Context, s state.State, service types.SrvName, a
 	case types.SrvChassis:
 		leaveChassis(ctx, s)
 	case types.SrvBgp:
-		err = bgp.DisableService()
+		err = bgp.DisableService(ctx, s)
 	default:
 		deactivateService(service, true)
 	}
