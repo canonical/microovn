8a8992f6c8af7ba836bbb9a548b833410ada49b0
MJ Ponsonby <mj.ponsonby@canonical.com>
2025-04-11 18:33:21 +0100

    MicroOVN Paths implementation change to map
    Changes the MicroOVN paths implementation to a map based implementation
    instead of the switch lookup, this means in a 0 argument situation you
    can iterate over all of the items and print them.
    
    This makes using the paths command much easier.
    
    Signed-off-by: MJ Ponsonby <mj.ponsonby@canonical.com>

--- microovn/cmd/microovn/paths.go
+++ microovn/cmd/microovn/paths.go
@@ -16,16 +16,23 @@ func (c *cmdPath) Command() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "path <NAME>",
 		Short: "Retrieve the path for the given name.",
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.RangeArgs(0, 1),
 		RunE:  c.Run,
 	}
 	return cmd
 }
 
 func (c *cmdPath) Run(_ *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		for k, v := range paths.PathsMap {
+			fmt.Printf("%s: %s\n", k, v)
+		}
+		return nil
+	}
+
 	name := args[0]
-	path := paths.GetPath(name)
-	if path == nil {
+	path, exists := paths.PathsMap[name]
+	if !exists {
 		return fmt.Errorf("unknown path name: %s", name)
 	}
 	fmt.Println(path)
