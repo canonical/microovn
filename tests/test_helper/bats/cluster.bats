canonical/microovn

commit ad657d813db24fad950171ddd454292f7b9d5966

Refactor of service control functionality
Makes enabling chassis work better and now the join/leave/bootstrap
functions enable their services with node enable/disable functionality

Signed-off-by: MJ Ponsonby <mj.ponsonby@canonical.com>

microovn/node/node.go
@@ -21,6 +21,10 @@ import (
 
 // DisableService - stop snap service(s) (runtime state) and remove it from the
 // database (desired state).
+//
+// NOTE: this function does not update the environment file,
+// if central is disabled then the environment files for the other nodes will be
+// incorrect, please call with a method of updating the clusters env files afterwards
 func DisableService(ctx context.Context, s state.State, service types.SrvName) error {
 	exists, err := HasServiceActive(ctx, s, service)
 
@@ -31,6 +35,10 @@ func DisableService(ctx context.Context, s state.State, service types.SrvName) e
 		return errors.New("This service is not enabled")
 	}
 
+	// If going to disable central, check if possible, this is done before the
+	// other check if central because we need to do a database transaction,
+	// and if this check is moved into the later "if central" then we
+	// will need to handle the database check on each branch of the if
 	if service == types.SrvCentral {
 		centrals, err := FindService(ctx, s, service)
 		if err != nil {
@@ -39,29 +47,36 @@ func DisableService(ctx context.Context, s state.State, service types.SrvName) e
 		if len(centrals) == 1 {
 			return errors.New("You cannot delete the final enabled central service")
 		}
-		err = LeaveCentral(ctx, s)
-		if err != nil {
-			return err
-		}
-	} else {
-		err = snap.Stop(service, true)
-	}
-
-	if err != nil {
-		return fmt.Errorf("Snapctl error, likely due to service not existing:\n %w", err)
 	}
 
 	err = s.Database().Transaction(ctx, func(ctx context.Context, tx *sql.Tx) error {
 		err := database.DeleteService(ctx, tx, s.Name(), service)
 		return err
 	})
+	if err != nil {
+		return err
+	}
+
+	if service == types.SrvCentral {
+		err = leaveCentral(ctx, s)
+	} else if service == types.SrvChassis {
+		err = leaveChassis(ctx, s)
+	} else {
+		err = snap.Stop(service, true)
+		if err != nil {
+			err = fmt.Errorf("Snapctl error, likely due to service not existing:\n %w", err)
+		}
+	}
 
 	return err
-
 }
 
 // EnableService - start snap service(s) (runtime state) and add it to the
 // database (desired state).
+//
+// NOTE: this function does not update the environment file,
+// if central is enabled then the environment files for the other nodes will be
+// incorrect, please call with a method of updating the clusters env files afterwards
 func EnableService(ctx context.Context, s state.State, service types.SrvName) error {
 	exists, err := HasServiceActive(ctx, s, service)
 	if err != nil {
@@ -74,25 +89,27 @@ func EnableService(ctx context.Context, s state.State, service types.SrvName) er
 	if !types.CheckValidService(service) {
 		return errors.New("Service does not exist")
 	}
-	if service == types.SrvCentral {
-		err = JoinCentral(ctx, s)
-		if err != nil {
-			return err
-		}
-	} else {
-		err = snap.Start(service, true)
-		if err != nil {
-			return fmt.Errorf("Snapctl error, likely due to service not existing:\n%w", err)
-		}
-	}
 
 	err = s.Database().Transaction(ctx, func(ctx context.Context, tx *sql.Tx) error {
 		_, err := database.CreateService(ctx, tx, database.Service{Member: s.Name(), Service: service})
 		return err
 	})
+	if err != nil {
+		return err
+	}
+
+	if service == types.SrvCentral {
+		err = joinCentral(ctx, s)
+	} else if service == types.SrvChassis {
+		err = joinChassis(ctx, s)
+	} else {
+		err = snap.Start(service, true)
+		if err != nil {
+			err = fmt.Errorf("Snapctl error, likely due to service not existing:\n%w", err)
+		}
+	}
 
 	return err
-
 }
 
 // ListServices - List services in database (desired state).
@@ -197,10 +214,10 @@ func ServiceWarnings(ctx context.Context, s state.State) (types.WarningSet, erro
 	return output, nil
 }
 
-// JoinCentral safely starts the central services child services while also
+// joinCentral safely starts the central services child services while also
 // generating certificates to ensure secure connection with other central
 // nodes in the database
-func JoinCentral(ctx context.Context, s state.State) error {
+func joinCentral(ctx context.Context, s state.State) error {
 	// Generate certificate for OVN Central services
 	err := certificates.GenerateNewServiceCertificate(ctx, s, "ovnnb", certificates.CertificateTypeServer)
 	if err != nil {
@@ -232,9 +249,9 @@ func JoinCentral(ctx context.Context, s state.State) error {
 	return nil
 }
 
-// LeaveCentral safely stops the central service's child services, and leaves
+// leaveCentral safely stops the central service's child services, and leaves
 // the central database cluster safely.
-func LeaveCentral(ctx context.Context, s state.State) error {
+func leaveCentral(ctx context.Context, s state.State) error {
 	// Leave SB and NB clusters
 	logger.Info("Leaving OVN Northbound cluster")
 	_, err := ovnCmd.AppCtl(ctx, s, paths.OvnNBControlSock(), "cluster/leave", "OVN_Northbound")
@@ -282,6 +299,47 @@ func LeaveCentral(ctx context.Context, s state.State) error {
 	err = snap.Stop("ovn-ovsdb-server-sb", true)
 	if err != nil {
 		logger.Warnf("Failed to stop OVN SB service: %s", err)
+	}
+	return nil
+}
+
+func leaveChassis(ctx context.Context, s state.State) error {
+	chassisName := s.Name()
+
+	// Gracefully exit OVN controller causing chassis to be automatically removed.
+	logger.Infof("Stopping OVN Controller and removing Chassis '%s' from OVN SB database.", chassisName)
+	_, err := ovnCmd.ControllerCtl(ctx, s, "exit")
+	if err != nil {
+		logger.Warnf("Failed to gracefully stop OVN Controller: %s", err)
+	}
+
+	err = snap.Stop(types.SrvChassis, true)
+	if err != nil {
+		logger.Warnf("Failed to stop Chassis service: %s", err)
+	}
+	return nil
+}
+
+func joinChassis(ctx context.Context, s state.State) error {
+	// Generate certificate for OVN chassis (controller)
+	err := certificates.GenerateNewServiceCertificate(ctx, s, "ovn-controller", certificates.CertificateTypeServer)
+	if err != nil {
+		return fmt.Errorf("failed to generate TLS certificate for ovn-controller service")
+	}
+	err = snap.Start("chassis", true)
+	if err != nil {
+		return fmt.Errorf("Failed to start OVN chassis: %w", err)
+	}
+	return nil
+}
+
+// DisableAllServices is a function to disable alot of services
+func DisableAllServices(ctx context.Context, s state.State) error {
+	for _, service := range types.ServiceNames {
+		err := DisableService(ctx, s, service)
+		if err != nil {
+			logger.Warnf("%s", err)
+		}
 	}
 	return nil
 }

microovn/cmd/microovn/certificates_list.go
@@ -101,7 +101,7 @@ func (c *cmdCertificatesList) Run(cmd *cobra.Command, _ []string) error {
 			expectedCertificates.Northd = &certBundle{northdCert, northdKey}
 		}
 
-		if srv.Service == types.SrvChassis {
+		if srv.Service == types.SrvSwitch {
 			ctlCert, ctlKey := paths.PkiOvnControllerCertFiles()
 			expectedCertificates.Chassis = &certBundle{ctlCert, ctlKey}
 		}

microovn/ovn/bootstrap.go
@@ -2,15 +2,15 @@ package ovn
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 
+	"github.com/canonical/lxd/shared/logger"
 	"github.com/canonical/microcluster/v2/state"
 
-	"github.com/canonical/microovn/microovn/database"
+	"github.com/canonical/microovn/microovn/api/types"
+	"github.com/canonical/microovn/microovn/node"
 	"github.com/canonical/microovn/microovn/ovn/certificates"
 	ovnCmd "github.com/canonical/microovn/microovn/ovn/cmd"
-	"github.com/canonical/microovn/microovn/snap"
 )
 
 // Bootstrap will initialize a new OVN deployment.
@@ -21,30 +21,6 @@ func Bootstrap(ctx context.Context, s state.State, initConfig map[string]string)
 
 	// Create our storage.
 	err := createPaths()
-	if err != nil {
-		return err
-	}
-
-	// Record the new roles in the database.
-	err = s.Database().Transaction(ctx, func(ctx context.Context, tx *sql.Tx) error {
-		// Record the roles.
-		_, err := database.CreateService(ctx, tx, database.Service{Member: s.Name(), Service: "switch"})
-		if err != nil {
-			return fmt.Errorf("Failed to record role: %w", err)
-		}
-
-		_, err = database.CreateService(ctx, tx, database.Service{Member: s.Name(), Service: "central"})
-		if err != nil {
-			return fmt.Errorf("Failed to record role: %w", err)
-		}
-
-		_, err = database.CreateService(ctx, tx, database.Service{Member: s.Name(), Service: "chassis"})
-		if err != nil {
-			return fmt.Errorf("Failed to record role: %w", err)
-		}
-
-		return nil
-	})
 	if err != nil {
 		return err
 	}
@@ -75,52 +51,29 @@ func Bootstrap(ctx context.Context, s state.State, initConfig map[string]string)
 		return fmt.Errorf("failed to generate TLS certificate for client: %s", err)
 	}
 
-	// Enable OVS switch.
-	err = snap.Start("switch", true)
+	// Start all the required services
+
+	err = node.EnableService(ctx, s, types.SrvSwitch)
 	if err != nil {
-		return fmt.Errorf("Failed to start OVS switch: %w", err)
+		logger.Infof("failed to enable switch")
+		return err
 	}
 
-	// Generate certificate for OVN Central services
-	err = certificates.GenerateNewServiceCertificate(ctx, s, "ovnnb", certificates.CertificateTypeServer)
+	err = node.EnableService(ctx, s, types.SrvCentral)
 	if err != nil {
-		return fmt.Errorf("failed to generate TLS certificate for ovnnb service: %s", err)
-	}
-	err = certificates.GenerateNewServiceCertificate(ctx, s, "ovnsb", certificates.CertificateTypeServer)
-	if err != nil {
-		return fmt.Errorf("failed to generate TLS certificate for ovnsb service: %s", err)
-	}
-	err = certificates.GenerateNewServiceCertificate(ctx, s, "ovn-northd", certificates.CertificateTypeServer)
-	if err != nil {
-		return fmt.Errorf("failed to generate TLS certificate for ovn-northd service: %s", err)
+		logger.Infof("failed to enable central")
+		return err
 	}
 
-	// Enable OVN central.
-	err = snap.Start("ovn-ovsdb-server-nb", true)
+	err = generateEnvironment(ctx, s)
 	if err != nil {
-		return fmt.Errorf("Failed to start OVN NB: %w", err)
+		return fmt.Errorf("Failed to generate the daemon configuration: %w", err)
 	}
 
-	err = snap.Start("ovn-ovsdb-server-sb", true)
+	err = node.EnableService(ctx, s, types.SrvChassis)
 	if err != nil {
-		return fmt.Errorf("Failed to start OVN SB: %w", err)
-	}
-
-	err = snap.Start("ovn-northd", true)
-	if err != nil {
-		return fmt.Errorf("Failed to start OVN northd: %w", err)
-	}
-
-	// Generate certificate for OVN chassis (controller)
-	err = certificates.GenerateNewServiceCertificate(ctx, s, "ovn-controller", certificates.CertificateTypeServer)
-	if err != nil {
-		return fmt.Errorf("failed to generate TLS certificate for ovn-controller service: %s", err)
-	}
-
-	// Enable OVN chassis.
-	err = snap.Start("chassis", true)
-	if err != nil {
-		return fmt.Errorf("Failed to start OVN chassis: %w", err)
+		logger.Infof("failed to enable switch")
+		return err
 	}
 
 	// Configure OVS to use OVN.

microovn/ovn/join.go
@@ -5,13 +5,14 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/canonical/lxd/shared/logger"
 	"github.com/canonical/microcluster/v2/state"
 
+	"github.com/canonical/microovn/microovn/api/types"
 	"github.com/canonical/microovn/microovn/database"
 	"github.com/canonical/microovn/microovn/node"
 	"github.com/canonical/microovn/microovn/ovn/certificates"
 	ovnCmd "github.com/canonical/microovn/microovn/ovn/cmd"
-	"github.com/canonical/microovn/microovn/snap"
 )
 
 // Join will join an existing OVN deployment.
@@ -44,32 +45,6 @@ func Join(ctx context.Context, s state.State, initConfig map[string]string) erro
 		return err
 	}
 
-	// Record the new roles in the database.
-	err = s.Database().Transaction(ctx, func(ctx context.Context, tx *sql.Tx) error {
-		// Record the roles.
-		_, err := database.CreateService(ctx, tx, database.Service{Member: s.Name(), Service: "switch"})
-		if err != nil {
-			return fmt.Errorf("Failed to record role: %w", err)
-		}
-
-		if srvCentral < 3 {
-			_, err = database.CreateService(ctx, tx, database.Service{Member: s.Name(), Service: "central"})
-			if err != nil {
-				return fmt.Errorf("Failed to record role: %w", err)
-			}
-		}
-
-		_, err = database.CreateService(ctx, tx, database.Service{Member: s.Name(), Service: "chassis"})
-		if err != nil {
-			return fmt.Errorf("Failed to record role: %w", err)
-		}
-
-		return nil
-	})
-	if err != nil {
-		return err
-	}
-
 	// Generate the configuration.
 	err = generateEnvironment(ctx, s)
 	if err != nil {
@@ -91,28 +66,30 @@ func Join(ctx context.Context, s state.State, initConfig map[string]string) erro
 		return err
 	}
 
-	// Enable OVS switch.
-	err = snap.Start("switch", true)
+	// Start all the required services, and central if needed
+	err = node.EnableService(ctx, s, types.SrvSwitch)
 	if err != nil {
-		return fmt.Errorf("Failed to start OVS switch: %w", err)
+		logger.Infof("failed to enable switch")
+		return err
 	}
 
-	// Enable OVN central (if needed).
 	if srvCentral < 3 {
-		err = node.JoinCentral(ctx, s)
+		err = node.EnableService(ctx, s, types.SrvCentral)
 		if err != nil {
-			return fmt.Errorf("Failed to start OVN central: %w", err)
+			logger.Infof("failed to enable central")
+			return err
 		}
 	}
 
-	// Generate certificate for OVN chassis (controller)
-	err = certificates.GenerateNewServiceCertificate(ctx, s, "ovn-controller", certificates.CertificateTypeServer)
+	err = generateEnvironment(ctx, s)
 	if err != nil {
-		return fmt.Errorf("failed to generate TLS certificate for ovn-controller service")
+		return fmt.Errorf("Failed to generate the daemon configuration: %w", err)
 	}
-	err = snap.Start("chassis", true)
+
+	err = node.EnableService(ctx, s, types.SrvChassis)
 	if err != nil {
-		return fmt.Errorf("Failed to start OVN chassis: %w", err)
+		logger.Infof("failed to enable switch")
+		return err
 	}
 
 	// Enable OVN chassis.

microovn/ovn/leave.go
@@ -7,8 +7,6 @@ import (
 	"github.com/canonical/microcluster/v2/state"
 
 	"github.com/canonical/microovn/microovn/node"
-	ovnCmd "github.com/canonical/microovn/microovn/ovn/cmd"
-	"github.com/canonical/microovn/microovn/snap"
 )
 
 // Leave function gracefully departs from the OVN cluster before the member is removed from MicroOVN
@@ -21,29 +19,10 @@ import (
 // for departing cluster member, so we'll try to exit/leave/stop all possible services
 // ignoring any errors from services that are not actually running.
 func Leave(ctx context.Context, s state.State, _ bool) error {
-	var err error
-	chassisName := s.Name()
-
-	// Gracefully exit OVN controller causing chassis to be automatically removed.
-	logger.Infof("Stopping OVN Controller and removing Chassis '%s' from OVN SB database.", chassisName)
-	_, err = ovnCmd.ControllerCtl(ctx, s, "exit")
+	// Attempt to disable each service
+	err := node.DisableAllServices(ctx, s)
 	if err != nil {
-		logger.Warnf("Failed to gracefully stop OVN Controller: %s", err)
-	}
-
-	err = snap.Stop("chassis", true)
-	if err != nil {
-		logger.Warnf("Failed to stop Chassis service: %s", err)
-	}
-
-	err = snap.Stop("switch", true)
-	if err != nil {
-		logger.Warnf("Failed to stop Switch service: %s", err)
-	}
-
-	err = node.LeaveCentral(ctx, s)
-	if err != nil {
-		logger.Warnf("Failed to stop Central service: %s", err)
+		return err
 	}
 
 	logger.Info("Cleaning up runtime and data directories.")

commit e0268bd9fa2c90cdf031c2f2c206669413e1b4c0

node: Fix incomplete remote addresses in northd
MicroOVN used incomplete list of NB/SB cluster members when
starting the ovn-northd service. When central service was enabled,
local ovn.env file, containing list of IP addresses of NB/SB
cluster members, was not updated before starting of the northd
service. This caused northd service, on a node that just enabled
the central service (or joined the cluster), to miss its own IP address.

This issue was partially hidden by the fact that all other northd
services in the cluster had complete list of remote IPs. It was
only the last joining member that was incomplete.
For the problem to manifest fully, the northd instance with the
incomplete list of remote addresses would have to aquire lock, and
the NB/SB database that was missing from its list would have to
become the cluster leader.

A new test was added to ensure that all northd instances in the
cluster have complete list of IPs of the NB/SB cluster members.

Fixes: ad657d813db24fad950171ddd454292f7b9d5966
Signed-off-by: Martin Kalcok <martin.kalcok@canonical.com>

microovn/node/node.go
@@ -17,6 +17,7 @@ import (
 	"github.com/canonical/microovn/microovn/ovn/certificates"
 	ovnCluster "github.com/canonical/microovn/microovn/ovn/cluster"
 	ovnCmd "github.com/canonical/microovn/microovn/ovn/cmd"
+	"github.com/canonical/microovn/microovn/ovn/environment"
 	"github.com/canonical/microovn/microovn/ovn/paths"
 	"github.com/canonical/microovn/microovn/snap"
 )
@@ -103,6 +104,11 @@ func EnableService(ctx context.Context, s state.State, service types.SrvName) er
 	})
 	if err != nil {
 		return err
+	}
+
+	err = environment.GenerateEnvironment(ctx, s)
+	if err != nil {
+		return fmt.Errorf("failed to regenerate environment file after enabling central service: %w", err)
 	}
 
 	switch service {

tests/test_helper/bats/cluster.bats
@@ -24,6 +24,9 @@ cluster_register_test_functions() {
             -- cluster_test_db_connection_string "$db"
     done
     bats_test_function \
+        --description "Northd connection string" \
+        -- cluster_test_northd_connection_string
+    bats_test_function \
         --description "Expected MicroOVN cluster count" \
         -- cluster_expected_count
     bats_test_function \
@@ -207,6 +210,42 @@ cluster_test_db_connection_string() {
             # By using a fully qualified search string we can safely use
             # partial matching.
             assert_output -p "ssl:${expected_addr}:${expected_port}"
+        done
+    done
+}
+
+# cluster_test_northd_connection_string
+#
+# Test that northd service is connected to all expected NB and SB database
+# cluster members.
+cluster_test_northd_connection_string() {
+    local cluster_addresses=()
+    readarray \
+        -t cluster_addresses \
+        < <(microovn_get_member_cluster_address "central" $TEST_CONTAINERS)
+
+    local container
+    for container in $TEST_CONTAINERS; do
+        local container_services
+        container_services=$(microovn_get_cluster_services)
+        if [[ "$container_services" != *"central"* ]]; then
+            echo "Skip $container, no central services" >&3
+            continue
+        fi
+
+        local northd_pid
+        northd_pid=$(microovn_get_service_pid "$container" "ovn-northd" "ovn")
+        run lxc_exec \
+            "$container" \
+            "ps -ww -o cmd -p $northd_pid"
+
+        for addr in "${cluster_addresses[@]}"; do
+            local expected_addr
+            expected_addr=$(print_address $addr)
+            # By using a fully qualified search string we can safely use
+            # partial matching.
+            assert_output -p "ssl:${expected_addr}:6641"
+            assert_output -p "ssl:${expected_addr}:6642"
         done
     done
 }