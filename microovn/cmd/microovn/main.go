5849d7f4306da0f46346a2d197a4e94749422832
Martin Kalcok <martin.kalcok@canonical.com>
2025-08-18 16:47:08 +0200

    daemon: Change default listening port
    The old default port of MicroOVN daemon (6443) conflicts with
    the default port of Kubernetes API [0]. Therefore, we are picking
    a new default of 6686. Brief search did not turn up any application
    commonly using this port, and it is currently marked as 'Unassigned'
    in the IANA list [1].
    
    Note this change affects only new installations. Any MicroOVN instances
    deployed from prior versions will retain the old port binding.
    
    [0] https://kubernetes.io/docs/concepts/security/controlling-access/#transport-security
    [1] https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.txt
    
    Signed-off-by: Martin Kalcok <martin.kalcok@canonical.com>
    (cherry picked from commit e3d03c0bd573a119ca173dd99614121520cc9be8)

--- microovn/cmd/microovn/main.go
+++ microovn/cmd/microovn/main.go
@@ -15,7 +15,7 @@ import (
 
 // DefaultMicroClusterPort is the default port used for the MicroOVN
 // MicroCluster Daemon.
-const DefaultMicroClusterPort = 6443
+const DefaultMicroClusterPort = 6686
 
 // CmdControl has functions that are common to the microctl commands.
 // command line tools.
