canonical/microovn

commit bb9b2b0c7d662477a2e85ba59d0d0d7014358f17

ovn.cmd: Add support for running nbctl on cluster
Current implementation of `NBCtl` function supports only local
unix socket communication. This change splits adds a `NBCtlCluster`
function that connects to the network endpoints of the NB cluster.
This function utilizes snap's 'ovn-nbctl' command wrapper to automatically
set correct network endpoints and TLS certificates.

Signed-off-by: MJ Ponsonby <mj.ponsonby@canonical.com>
Signed-off-by: Martin Kalcok <martin.kalcok@canonical.com>

microovn/ovn/cmd/commands.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"slices"
 	"strconv"
 
@@ -153,8 +154,9 @@ func ovnDBCtl(ctx context.Context, s state.State, dbType OvsdbType, timeout int,
 	return "", err
 }
 
-// NBCtl is a convenience function for execution of ovn-nbctl command which is
-// re-tried up to 3 times. If command arguments do not specify timeout (-t or
+// NBCtl is a convenience function for execution of ovn-nbctl command against
+// OVN NB local unix socket. The command is re-tried up to 3 times.
+// If command arguments do not specify timeout (-t or
 // --timeout), a default of 30s will be added automatically. Parameter "args"
 // is a list of arguments that are passed directly to the shell command.
 // Before the command is executed, this function ensures that underlying
@@ -165,8 +167,33 @@ func NBCtl(ctx context.Context, s state.State, args ...string) (string, error) {
 	return ovnDBCtl(ctx, s, OvsdbTypeNBLocal, DefaultDBConnectWait, args...)
 }
 
-// SBCtl is a convenience function for execution of ovn-sbctl command which is
-// re-tried up to 3 times. If command arguments do not specify timeout (-t or
+// NBCtlCluster is a convenience function for execution of ovn-nbctl command
+// against OVN NB cluster endpoints. The command is re-tried up to 3 times.
+// If command arguments do not specify timeout (-t or
+// --timeout), a default of 30s will be added automatically. Parameter "args"
+// is a list of arguments that are passed directly to the shell command.
+//
+// Warning: This function will fail if local MicroOVN node is not bootstrapped.
+func NBCtlCluster(ctx context.Context, args ...string) (string, error) {
+	if !slices.Contains(args, "--timeout") && !slices.Contains(args, "-t") {
+		args = append([]string{"--timeout", "30"}, args...)
+	}
+
+	var err error
+	// try command 3 times if it is failing
+	for attempts := 0; attempts < 3; attempts++ {
+		var output string
+		output, err = shared.RunCommandContext(ctx, filepath.Join(paths.Wrappers(), "ovn-nbctl"), args...)
+		if err == nil {
+			return output, nil
+		}
+	}
+	return "", err
+}
+
+// SBCtl is a convenience function for execution of ovn-sbctl command against
+// OVN NB local unix socket. The command is re-tried up to 3 times.
+// If command arguments do not specify timeout (-t or
 // --timeout), a default of 30s will be added automatically. Parameter "args" is
 // a list of arguments that are passed directly to the shell command. Before the
 // command is executed, this function ensures that underlying database is in

microovn/ovn/paths/paths.go
@@ -149,6 +149,9 @@ func OvsdbSwitchSchema() string {
 	return filepath.Join(snapRoot, "share", "openvswitch", "vswitch.ovsschema")
 }
 
+// Wrappers returns path to a directory with snap's command wrappers
+func Wrappers() string { return filepath.Join(snapRoot, "commands") }
+
 // FrrConfigDir returns path to a directory that FRR uses to store configuration
 func FrrConfigDir() string {
 	return filepath.Join(dataDir, "frr", "etc")

commit c24a7ab7e4314940d7d8735d2ca0716b75e4225a

commands: Wait for clustered DB to be in connected state.
`NBCtlCluster` is a helper that runs `ovn-nbctl` commands against the current
Northbound DB cluster. However, unlike its local counterpart (`NBCtl`), it
did not wait on healthy database state before executing commands.
This could result in occasional failures due to DB not being ready.

Unfortunately, the `ovsdb-client` has a bug [0] that prevents it
from accepting multiple targets at once, so the implementation performs
a manual check of individual cluster nodes.

[0] https://bugs.launchpad.net/ubuntu/+source/openvswitch/+bug/2127931

Fixes: bb9b2b0c7d66 ("ovn.cmd: Add support for running nbctl on cluster")
Signed-off-by: Martin Kalcok <martin.kalcok@canonical.com>

microovn/bgp/redirect.go
@@ -212,6 +212,7 @@ func createExternalNetworks(ctx context.Context, s state.State, extConnections [
 	// Create Logical Router
 	lrName := getLrName(s)
 	_, err := ovnCmd.NBCtlCluster(ctx,
+		s,
 		"--",
 		"lr-add", lrName,
 		"--",
@@ -234,6 +235,7 @@ func createExternalNetworks(ctx context.Context, s state.State, extConnections [
 		lrpMac := generateLrpMac(lrpName)
 
 		_, err = ovnCmd.NBCtlCluster(ctx,
+			s,
 			"--",
 			// Create Logical Router Port
 			"lrp-add", lrName, lrpName, lrpMac,
@@ -276,6 +278,7 @@ func createVrf(ctx context.Context, s state.State, extConnections []types.BgpExt
 	lrName := getLrName(s)
 
 	_, err := ovnCmd.NBCtlCluster(ctx,
+		s,
 		"set", "Logical_Router", lrName,
 		"options:dynamic-routing=true",
 		fmt.Sprintf("options:requested-tnl-key=%s", tableID),
@@ -287,6 +290,7 @@ func createVrf(ctx context.Context, s state.State, extConnections []types.BgpExt
 	for _, extConnection := range extConnections {
 		lrpName := getLrpName(s, extConnection.Iface)
 		_, err = ovnCmd.NBCtlCluster(ctx,
+			s,
 			"lrp-set-options", lrpName,
 			"dynamic-routing-maintain-vrf=true",
 			"dynamic-routing-redistribute=nat,lb",
@@ -371,6 +375,7 @@ func redirectBgp(ctx context.Context, s state.State, extConnections []types.BgpE
 
 		// Create Logical Switch Port to which the BGP+BFD traffic will be redirected
 		_, err := ovnCmd.NBCtlCluster(ctx,
+			s,
 			"--",
 			"lsp-add", lsName, bgpLsp,
 			"--",
@@ -422,7 +427,7 @@ func teardownAll(ctx context.Context, s state.State) error {
 	var allErrors error
 	// Find and remove Logical Router used for BGP redirect
 	logicalRouter := getLrName(s)
-	_, err := ovnCmd.NBCtlCluster(ctx, "lr-del", logicalRouter)
+	_, err := ovnCmd.NBCtlCluster(ctx, s, "lr-del", logicalRouter)
 	if err != nil {
 		allErrors = errors.Join(allErrors, fmt.Errorf("failed to delete Logical Router '%s': %v", logicalRouter, err))
 	}
@@ -433,7 +438,7 @@ func teardownAll(ctx context.Context, s state.State) error {
 	}
 
 	// Find and remove Logical Switches used to connect to external networks on the local chassis
-	logicalSwitches, err := ovnCmd.NBCtlCluster(ctx, "--bare", "--columns", "name",
+	logicalSwitches, err := ovnCmd.NBCtlCluster(ctx, s, "--bare", "--columns", "name",
 		"find", "logical_switch", fmt.Sprintf("external-ids:%s=true", BgpManagedTag),
 	)
 	if err != nil {
@@ -445,7 +450,7 @@ func teardownAll(ctx context.Context, s state.State) error {
 			if !strings.HasPrefix(logicalSwitch, chassisSwitchNamePrefix) {
 				continue
 			}
-			_, err = ovnCmd.NBCtlCluster(ctx, "ls-del", logicalSwitch)
+			_, err = ovnCmd.NBCtlCluster(ctx, s, "ls-del", logicalSwitch)
 			if err != nil {
 				allErrors = errors.Join(allErrors, fmt.Errorf("failed to delete Logical Switch '%s': %v", logicalSwitch, err))
 			}

microovn/ovn/cmd/commands.go
@@ -10,7 +10,9 @@ import (
 	"strconv"
 
 	"github.com/canonical/lxd/shared"
+	"github.com/canonical/lxd/shared/logger"
 	"github.com/canonical/microcluster/v2/state"
+	"github.com/canonical/microovn/microovn/ovn/environment"
 
 	"github.com/canonical/microovn/microovn/ovn/paths"
 )
@@ -109,6 +111,47 @@ func WaitForDBState(ctx context.Context, _ state.State, db *OvsdbSpec, dbState s
 	return nil
 }
 
+// WaitForClusterDBState checks state of the database in the "central" cluster. It iterates over the hosts in the
+// OVN central cluster, tries to connect to the clustered database at the specified 'port' and waits until the
+// database 'db' is in the expected state ('dbState').
+// The function returns nil on success, soon as it finds the expected state on at least one cluster member.
+//
+// Argument `db` is usually either `OVN_Northbound` or `OVN_Southbound`
+// Argument 'port' is usually '6641' for the Northbound database and `6642` for the Southbound database
+func WaitForClusterDBState(ctx context.Context, s state.State, db string, dbState string, port int) error {
+	var err error
+	nbIPs, err := environment.CentralIps(ctx, s)
+	if err != nil {
+		return fmt.Errorf("failed to get central IPs: %v", err)
+	}
+
+	// The manual iteration over individual cluster members is a workaround for a bug in the ovsdb-client
+	// https://bugs.launchpad.net/ubuntu/+source/openvswitch/+bug/2127931
+	dbConnected := false
+	for _, ip := range nbIPs {
+		socketURL := fmt.Sprintf("ssl:%s:%d", ip, port)
+		_, err = shared.RunCommandContext(
+			ctx,
+			filepath.Join(paths.Wrappers(), "ovsdb-client"),
+			"-t", "10",
+			"wait",
+			socketURL,
+			db,
+			dbState,
+		)
+		if err != nil {
+			logger.Warnf("Failed to connect to %s database at %s: %v", db, socketURL, err)
+		} else {
+			dbConnected = true
+			break
+		}
+	}
+	if !dbConnected {
+		return fmt.Errorf("failed to connect to %s database cluster", db)
+	}
+	return nil
+}
+
 // ovnDBCtl is a helper function to execute "ovn-nbctl" and "ovn-sbctl" commands
 // which are re-tried up to 3 times. If command arguments do not specify timeout (-t or
 // --timeout), a default of 30s will be added automatically. It takes "dbType"
@@ -174,12 +217,17 @@ func NBCtl(ctx context.Context, s state.State, args ...string) (string, error) {
 // is a list of arguments that are passed directly to the shell command.
 //
 // Warning: This function will fail if local MicroOVN node is not bootstrapped.
-func NBCtlCluster(ctx context.Context, args ...string) (string, error) {
+func NBCtlCluster(ctx context.Context, s state.State, args ...string) (string, error) {
 	if !slices.Contains(args, "--timeout") && !slices.Contains(args, "-t") {
 		args = append([]string{"--timeout", "30"}, args...)
 	}
 
 	var err error
+	err = WaitForClusterDBState(ctx, s, "OVN_Northbound", OvsdbConnected, 6641)
+	if err != nil {
+		return "", errors.New("failed to connect to OVN Northbound database cluster")
+	}
+
 	// try command 3 times if it is failing
 	for attempts := 0; attempts < 3; attempts++ {
 		var output string